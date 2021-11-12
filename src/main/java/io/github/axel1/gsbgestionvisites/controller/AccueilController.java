1e8a97525ca8aa7f51d395e811496d3e0dbfdc0e
Axel-1 <65816310+Axel-1@users.noreply.github.com>
2021-11-12 17:49:47 +0100

    Reformat code

--- src/main/java/io/github/axel1/gsbgestionvisites/controller/AccueilController.java
+++ src/main/java/io/github/axel1/gsbgestionvisites/controller/AccueilController.java
@@ -2,7 +2,6 @@ package io.github.axel1.gsbgestionvisites.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
